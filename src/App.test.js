94ba3daf84a4c80c30c7100e23a481289cefa8ef
lemonglutton <34936474+lemonglutton@users.noreply.github.com>
2023-04-15 17:02:53 +0200

    Example PR (#17)
    * Adding Tests when making PR workflow for main branch

--- src/App.test.js
+++ src/App.test.js
@@ -10,6 +10,6 @@ test('renders learn react link', () => {
 test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  expect(linkElement).toBeInTheDocument();  expect()
 });
 
